cc1ba3f2435964afee7fff0256f3f5e9b25fe45f
Robert <j091521@tafe.wa.edu.au>
2026-02-24 12:26:50 +0800

    wip(contact-us): Start of contact-us page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\StaticPages\ContactUsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contact-us', [ContactUsController::class, 'index'])
+    ->name('contact-us');
+
+Route::get('thankyou', [ContactUsController::class, 'thankyou'])
+    ->name('thankyou');
